bf95b6e2f866c13e3f6a4cffb5d85c809ded085d
Jaafar Barek <jaafarbarek@gmail.com>
2015-12-01 01:55:28 +0200

    Add STM32F411-Nucleo board

---
+++ micropython-1.5/stmhal/boards/STM32F411NUCLEO/mpconfigboard.h
@@ -0,0 +1,58 @@
+#define MICROPY_HW_BOARD_NAME       "F411NUCLEO"
+#define MICROPY_HW_MCU_NAME         "STM32F411"
+#define MICROPY_PY_SYS_PLATFORM     "pyboard"
+
+#define MICROPY_HW_HAS_SWITCH       (1)
+#define MICROPY_HW_HAS_SDCARD       (0)
+#define MICROPY_HW_HAS_MMA7660      (0)
+#define MICROPY_HW_HAS_LIS3DSH      (0)
+#define MICROPY_HW_HAS_LCD          (0)
+#define MICROPY_HW_ENABLE_RNG       (0)
+#define MICROPY_HW_ENABLE_RTC       (1)
+#define MICROPY_HW_ENABLE_TIMER     (1)
+#define MICROPY_HW_ENABLE_SERVO     (1)
+#define MICROPY_HW_ENABLE_DAC       (0)
+#define MICROPY_HW_ENABLE_SPI1      (1)
+#define MICROPY_HW_ENABLE_SPI2      (1)
+#define MICROPY_HW_ENABLE_SPI3      (0)
+#define MICROPY_HW_ENABLE_CAN       (0)
+
+// HSE is 8MHz
+#define MICROPY_HW_CLK_PLLM (5)
+#define MICROPY_HW_CLK_PLLN (210)
+#define MICROPY_HW_CLK_PLLP (RCC_PLLP_DIV4)
+#define MICROPY_HW_CLK_PLLQ (7)
+
+// Has a 32kHz crystal
+#define MICROPY_HW_RTC_USE_LSE      (1)
+
+// UART config
+//#define MICROPY_HW_UART1_PORT (GPIOA)
+//#define MICROPY_HW_UART1_PINS (GPIO_PIN_9 | GPIO_PIN_10)
+#define MICROPY_HW_UART2_PORT (GPIOA)
+#define MICROPY_HW_UART2_PINS (GPIO_PIN_2 | GPIO_PIN_3)
+//#define MICROPY_HW_UART2_RTS  (GPIO_PIN_1)
+//#define MICROPY_HW_UART2_CTS  (GPIO_PIN_0)
+
+// REPL on a hardware UART
+#define MICROPY_HW_UART_REPL        PYB_UART_2
+#define MICROPY_HW_UART_REPL_BAUD   115200
+
+// I2C busses
+#define MICROPY_HW_I2C1_SCL (pin_B8)
+#define MICROPY_HW_I2C1_SDA (pin_B9)
+
+// USRSW is pulled hight. Pressing the button makes the input go low.
+#define MICROPY_HW_USRSW_PIN        (pin_C13)
+#define MICROPY_HW_USRSW_PULL       (GPIO_NOPULL)
+#define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_FALLING)
+#define MICROPY_HW_USRSW_PRESSED    (0)
+
+// LEDs
+#define MICROPY_HW_LED1             (pin_D14) // red
+#define MICROPY_HW_LED2             (pin_D12) // green
+#define MICROPY_HW_LED3             (pin_D13) // orange
+#define MICROPY_HW_LED4             (pin_D15) // blue
+#define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
+#define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRL = pin->pin_mask)
+#define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRH = pin->pin_mask)
